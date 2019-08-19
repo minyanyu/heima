51394625c4e97b8aebdde6b04a7cc8cc44dbbc22
MIN <2443873776@qq.com>
2019-08-19 14:23:25 +0800

    ”新建登录和主页

---
+++ src/views/login/login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+登录页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+</style>
